c9705a9983a365fb830ff43dbe84864d7d2d7971
Czipperz <czipperz@gmail.com>
2017-02-12 15:18:43 -0800

    Cleanup Climb

--- src/org/usfirst/frc/team997/robot/commands/Climb.java
+++ src/org/usfirst/frc/team997/robot/commands/Climb.java
@@ -10,35 +10,25 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  *
  */
 public class Climb extends Command {
-
     public Climb() {
-        // Use requires() here to declare subsystem dependencies
-        // eg. requires(chassis);
     	requires(Robot.climber);
     }
 
-    // Called just before this Command runs the first time
-    protected void initialize() {
-    }
+    protected void initialize() {}
 
-    // Called repeatedly when this Command is scheduled to run
     protected void execute() {
     	double current = Robot.averageCurrent(RobotMap.PDP.climberTalon);
     	SmartDashboard.putNumber("Climber current", current);
     	Robot.climber.climb(RobotMap.Values.climbSpeed);
     }
 
-    // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
         return false;
     }
 
-    // Called once after isFinished returns true
     protected void end() {
     }
 
-    // Called when another command which requires one or more of the same
-    // subsystems is scheduled to run
     protected void interrupted() {
     }
 }
