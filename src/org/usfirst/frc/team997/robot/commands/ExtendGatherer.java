89f937043bbe8ba29b4fc3716d181643f307ceee
Czipperz <czipperz@gmail.com>
2017-02-12 17:47:09 -0800

    ExtendGatherer ends after 1 second rather than immediately

--- src/org/usfirst/frc/team997/robot/commands/ExtendGatherer.java
+++ src/org/usfirst/frc/team997/robot/commands/ExtendGatherer.java
@@ -2,21 +2,26 @@ package org.usfirst.frc.team997.robot.commands;
 
 import org.usfirst.frc.team997.robot.Robot;
 
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.Command;
 
 /**
  *
  */
 public class ExtendGatherer extends Command {
+	private Timer timer;
 
     public ExtendGatherer() {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
     	requires(Robot.gatherer);
+    	timer = new Timer();
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
+    	timer.reset();
+    	timer.start();
     }
 
     // Called repeatedly when this Command is scheduled to run
@@ -26,7 +31,7 @@ public class ExtendGatherer extends Command {
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return true;
+        return timer.get() > 1;
     }
 
     // Called once after isFinished returns true
