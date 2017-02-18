1e4f216af690bd668177a836631db31152a081eb
czipperz <czipperz@gmail.com>
2017-02-17 20:12:27 -0800

    Fixed saving error...

--- src/org/usfirst/frc/team997/robot/AHRSWrapper.java
+++ src/org/usfirst/frc/team997/robot/AHRSWrapper.java
@@ -1,5 +1,26 @@
 package org.usfirst.frc.team997.robot;
 
+import edu.wpi.first.wpilibj.PIDSource;
+import edu.wpi.first.wpilibj.PIDSourceType;
+
 public class AHRSWrapper implements PIDSource{
+	public double offset;
+	@Override
+	public void setPIDSourceType(PIDSourceType type) {
+		// TODO Auto-generated method stub
+		Robot.driveTrain.ahrs.setPIDSourceType(type);
+	}
+
+	@Override
+	public PIDSourceType getPIDSourceType() {
+		// TODO Auto-generated method stub
+		return Robot.driveTrain.ahrs.getPIDSourceType();
+	}
+
+	@Override
+	public double pidGet() {
+		// TODO Auto-generated method stub
+		return Robot.driveTrain.ahrs.pidGet() - offset;
+	}
 
 }
