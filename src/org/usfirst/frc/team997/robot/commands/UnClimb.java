47e30595c248b35588f560543e910ff9a91c1352
Czipperz <czipperz@gmail.com>
2017-03-01 16:19:26 -0800

    More climber stuff - commands

---
+++ src/org/usfirst/frc/team997/robot/commands/UnClimb.java
@@ -0,0 +1,49 @@
+package org.usfirst.frc.team997.robot.commands;
+
+import org.usfirst.frc.team997.robot.Robot;
+import org.usfirst.frc.team997.robot.RobotMap;
+
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class UnClimb extends Command {
+	private Timer timer;
+
+    public UnClimb() {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    	requires(Robot.climber);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    	timer = new Timer();
+    	timer.reset();
+    	timer.start();
+    	
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	Robot.climber.unClimb();
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        
+    	return (timer.get() > 2);
+
+    }
+    // Called once after isFinished returns true
+    protected void end() {
+    	Robot.climber.stopClimb();
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
