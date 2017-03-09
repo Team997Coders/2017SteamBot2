a418353f417b6a52bd58ab21be3441704a93a575
Czipperz <czipperz@gmail.com>
2017-03-08 17:17:54 -0800

    Fixed time for UnClimb command

--- src/org/usfirst/frc/team997/robot/commands/UnClimb.java
+++ src/org/usfirst/frc/team997/robot/commands/UnClimb.java
@@ -34,7 +34,7 @@ public class UnClimb extends Command {
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
         
-    	return (timer.get() > 2);
+    	return (timer.get() > 0.5);
 
     }
     // Called once after isFinished returns true
